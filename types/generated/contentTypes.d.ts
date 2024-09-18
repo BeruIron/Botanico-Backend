0e26e92ac98cbe3c06d32f5be3f68be53b77684b
vanntoengcaptaincambodia <toeng.vann@institute.pse.ngo>
2024-09-18 16:08:21 +0700

    config strapi

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -374,7 +374,7 @@ export interface ApiBlogsBlogs extends Schema.CollectionType {
     draftAndPublish: true;
   };
   attributes: {
-    Images: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
+    Images: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     title: Attribute.String;
     Date: Attribute.Date;
     text: Attribute.String & Attribute.Required;
